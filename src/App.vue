774dfd09800c4259d455b3a59ee0d726f21f7b07
Kolton Main <koltonmain2003@gmail.com>
2023-06-08 17:57:50 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Helvetica, Avenir, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body{
+  margin-left: 40px;
+  margin-right: 40px;
+  background: linear-gradient(90deg, #1B1B1B 0%, #373737 100%);
+  color: #9c9c9c;
+}
+</style>
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,41 @@
+<template>
+<router-link class="post" :to="{ name: 'Details', params: { slug: post.slug }}">
+  <div class="post">
+      {{ post.date }} - {{ post.title }}  <span>#{{ post.tag }}</span>
+  </div>
+  </router-link>
+</template>
+
+<script>
+import { } from 'vue'
+export default {
+    props: ['post'],
+}
+</script>
+
+<style>
+.post {
+    color: #9c9c9c;
+    padding: 10px 0px;
+    text-align: left;
+    display: fixed;
+    text-decoration: none;
+    margin: 0px;
+    font-size: 20px;
+    font-weight: bold;
+}
+.post a {
+    color: #bababa;
+    font-weight: bold;
+}
+.post span {
+  font-weight: normal;
+  color: #787878;
+}
+.post:hover {
+  border-top-style: solid;
+  border-bottom-style: solid;
+  border-top-width: 1px;
+  border-bottom-width: 1px;
+}
+</style>
